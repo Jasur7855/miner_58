8bd027b21106381450c3fd2600f0e7f8736cebba
Jasur <103939215+Jasur7855@users.noreply.github.com>
2024-12-11 21:44:47 +0500

    добавил регистрацию и логин игры сапёр

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const gameBtn = document.getElementById("gameBtn");
 
 //Слушатели событий
 gameBtn.addEventListener("click", activeArea);
+document.querySelector("#loginWrapper form").addEventListener("submit",(event)=>{
+  event.preventDefault()
+  auth()
+})
 
 function activeArea() {
   const field = document.getElementsByClassName("field");
@@ -12,5 +16,73 @@ function activeArea() {
     setInterval(() => {
       field[i].classList.add("active");
     }, 20 * i);
+  }
+}
+
+let USERNAME;
+
+async function auth() {
+  const loginWrapper = document.getElementById("loginWrapper");
+  const login = document.getElementById("login").value;
+  let response = await sendRequest("user", "GET", {
+    username: login,
+  });
+  if (response.error) {
+    // Пользователь не зарегестрировался
+    let registration = await sendRequest("user", "POST", { username: login });
+    if (registration.error) {
+      alert(registration.message);
+    } else {
+      USERNAME = login;
+      loginWrapper.style.display = "none";
+      updateUSerBalance();
+    }
+  } else {
+    USERNAME = login;
+    loginWrapper.style.display = "none";
+    updateUSerBalance();
+  }
+}
+
+async function updateUSerBalance() {
+  let response = await sendRequest("user", "GET", {
+    username: USERNAME,
+  });
+
+  if (response.error) {
+    // Если есть ошибка
+    alert(response.message);
+  } else {
+    const user = document.querySelector("header span");
+    user.innerHTML = `Пользователь ${response.username} с балансом ${response.balance}`;
+  }
+}
+
+async function sendRequest(url, method, data) {
+  url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`;
+
+  if (method == "POST") {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    response = await response.json();
+    return response;
+  } else if (method == "GET") {
+    url = url + "?" + new URLSearchParams(data);
+    let response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    return response;
   }
 }
