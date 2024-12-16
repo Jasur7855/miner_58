4d979e9f1ad71ed16c00e9427fd909fed6697081
Jasur <103939215+Jasur7855@users.noreply.github.com>
2024-12-16 15:58:39 +0500

    добавления функционала начала игры

--- main.js
+++ main.js
@@ -1,25 +1,100 @@
 // Кнопки
 const gameBtn = document.getElementById("gameBtn");
-
+let points = 1000;
+let USERNAME;
+let game_id;
 //Слушатели событий
-gameBtn.addEventListener("click", activeArea);
-document.querySelector("#loginWrapper form").addEventListener("submit",(event)=>{
-  event.preventDefault()
-  auth()
-})
+gameBtn.addEventListener("click", startOrStopGame);
+document
+  .querySelector("#loginWrapper form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    auth();
+  });
+
+[...document.getElementsByClassName("point")].forEach((elem) => {
+  elem.addEventListener("click", addPoint);
+});
+
+function addPoint(event) {
+  let target = event.target;
+  points = +target.innerHTML;
+  const activeBtn = document.querySelector(".point.active");
+  activeBtn.classList.remove("active");
+
+  target.classList.add("active");
+}
+
+function startOrStopGame() {
+  if (gameBtn.innerHTML == "ИГРАТЬ") {
+    // НАЧАТЬ ИГРУ
+    gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
+    gameBtn.style.backgroundColor = "red";
+    startGame();
+  } else {
+    // ЗАВЕРШИТЬ ИГРУ
+    gameBtn.innerHTML = "ИГРАТЬ";
+    gameBtn.style.backgroundColor = "#66a663";
+    stopGame();
+  }
+}
+async function startGame() {
+  console.log(points);
+  let response = await sendRequest("new_game", "POST", {
+    username: USERNAME,
+    points,
+  });
+  if (response.error) {
+    // ЕСТЬ ОШИБКА
+    gameBtn.innerHTML = "ИГРАТЬ";
+    alert(response.message);
+  } else {
+    game_id = response.game_id;
+    updateUSerBalance();
+    activeArea();
+  }
+}
+async function stopGame() {
+  let response = await sendRequest("stop_game", "POST", {
+    username: USERNAME,
+    game_id,
+  });
+  if (response.error) {
+    // ЕСТЬ ОШИБКА
+    gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
+    alert(response.message);
+  } else {
+    updateUSerBalance();
+    resetField();
+  }
+}
 
 function activeArea() {
   const field = document.getElementsByClassName("field");
-  gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ";
-  gameBtn.style.backgroundColor = "red";
+
   for (let i = 0; i < field.length; i++) {
-    setInterval(() => {
+    field[i].addEventListener("contextmenu", setFlag);
+    setTimeout(() => {
       field[i].classList.add("active");
     }, 20 * i);
   }
 }
+function setFlag(event) {
+  event.preventDefault();
+  let target = event.target;
+  target.classList.toggle("flag");
+}
 
-let USERNAME;
+function resetField() {
+  const gameField = document.querySelector(".gameField");
+  gameField.innerHTML = "";
+  for (let i = 0; i < 80; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("field");
+    gameField.appendChild(cell);
+  }
+}
+resetField();
 
 async function auth() {
   const loginWrapper = document.getElementById("loginWrapper");
